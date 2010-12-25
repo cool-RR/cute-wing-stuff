26adaa361784801d0954e8096d00c5f468d9a717
Ram Rachum <cool-rr@cool-rr.com>
2010-12-25 19:19:08 +0200

    -

--- scripts/arg_to_attr.py
+++ scripts/arg_to_attr.py
@@ -1,4 +1,13 @@
 import wingapi
 
-def arg_to_attr():
-    pass
+def arg_to_attr(editor=wingapi.kArgEditor):
+    assert isinstance(editor, wingapi.CAPIEditor)
+    document = editor.GetDocument()
+    assert isinstance(document, wingapi.CAPIDocument)
+    document.BeginUndoAction()
+    try:
+        #text = document.GetText()
+        document.SetText(str(editor.GetSelection()))
+    finally:
+        document.EndUndoAction()
+        
