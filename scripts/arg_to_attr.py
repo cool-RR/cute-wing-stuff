b4e6ff5165debebb9dd806c060aeb80c63859363
Ram Rachum <cool-rr@cool-rr.com>
2010-12-25 19:43:37 +0200

    -

--- scripts/arg_to_attr.py
+++ scripts/arg_to_attr.py
@@ -1,13 +1,26 @@
 import wingapi
 
+def _get_word(editor=wingapi.kArgEditor):
+    pass
+
 def arg_to_attr(editor=wingapi.kArgEditor):
     assert isinstance(editor, wingapi.CAPIEditor)
     document = editor.GetDocument()
     assert isinstance(document, wingapi.CAPIDocument)
     document.BeginUndoAction()
     try:
-        #text = document.GetText()
-        document.SetText(str(editor.GetSelection()))
+        editor.ExecuteCommand('backward-word')
+        editor.ExecuteCommand('forward-word')
+        editor.ExecuteCommand('backward-word')
+        editor.ExecuteCommand('forward-word-extend')
+        start, end = editor.GetSelection()
+        variable_name = document.GetCharRange(start, end)
+        result_string = 'self.%s = %s' % (variable_name, variable_name)
+        document.DeleteChars(start, end)
+        document.InsertChars(start, result_string)
+        editor.SetSelection(start + len(result_string),
+                            start + len(result_string))
+        editor.ExecuteCommand('new-line')
     finally:
         document.EndUndoAction()
         
