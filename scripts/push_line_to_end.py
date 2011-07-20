c5478eeb8f3329fa4b96a4f53dd7f5f17246380c
Ram Rachum <cool-rr@cool-rr.com>
2011-07-20 22:13:14 +0200

    -

--- scripts/push_line_to_end.py
+++ scripts/push_line_to_end.py
@@ -2,7 +2,7 @@
 # This program is distributed under the LGPL2.1 license.
 
 '''
-This module defines the `push_line_to_end` script. blocktododoc whole module
+This module defines the `push_line_to_end` script.
 
 See its documentation for more information.
 '''
@@ -12,15 +12,28 @@ from __future__ import with_statement
 
 import os.path, sys; sys.path.append(os.path.dirname(__file__))
 
-import inspect
-
 import wingapi
 
 import shared
 
 
+TARGET_LINE_LENGTH = 79
+'''
+The maximum number of characters that should be in one line.
+
+The user-customized number is available through Wing settings, but because of
+an off-by-one bug in Wing, it is hard-coded here.
+'''
+
+
 def push_line_to_end(editor=wingapi.kArgEditor):
     '''
+    Push the current line to the end, aligning it to right border of editor.
+    
+    This inserts or deletes as many spaces as necessary from the beginning of
+    the line to make the end of the line exactly coincide with the right border
+    of the editor. (Whose width can be configured in the `TARGET_LINE_LENGTH`
+    constant in this module.)
     '''
     assert isinstance(editor, wingapi.CAPIEditor)
     document = editor.GetDocument()
@@ -29,15 +42,26 @@ def push_line_to_end(editor=wingapi.kArgEditor):
     line = document.GetLineNumberFromPosition(position)
     line_start = document.GetLineStart(line)
     line_end = document.GetLineEnd(line)
-    raise Exception((line_start, line_end))
-    #with shared.UndoableAction(document):
-        #start, end = shared.select_current_word(editor)    
-        #variable_name = document.GetCharRange(start, end)
-        #result_string = 'self.%s = %s' % (variable_name, variable_name)
-        #document.DeleteChars(start, end - 1)
-        #document.InsertChars(start, result_string)
-        #editor.SetSelection(start + len(result_string),
-                            #start + len(result_string))
-        #editor.ExecuteCommand('new-line')
-    
-        
+    current_line_length = line_end - line_start
+    n_spaces_to_add = TARGET_LINE_LENGTH - current_line_length
+    if n_spaces_to_add == 0:
+        return
+    elif n_spaces_to_add > 0:
+        string_to_insert = (' ' * n_spaces_to_add)
+        with shared.UndoableAction(document):
+            document.InsertChars(line_start, string_to_insert)
+    else:
+        assert n_spaces_to_add < 0
+        line_content = document.GetCharRange(line_start, line_end)
+        n_spaces_to_delete = min(
+            -n_spaces_to_add,
+            shared.get_n_identical_edge_characters(
+                line_content,
+                character=' '
+            )
+        )    
+        with shared.UndoableAction(document):   
+            document.DeleteChars(
+                line_start,
+                line_start + (n_spaces_to_delete - 1)
+            )
