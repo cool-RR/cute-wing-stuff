b4c67e2f008f06ce46e244acb9252e7b9f059eb1
Ram Rachum <ram@rachum.com>
2012-05-08 22:21:31 +0200

    - (reverse-merged from commit 8da66dd0098706f3380474ce96aea5f804e74be8)

---
+++ scripts/cute_select_more.py
@@ -0,0 +1,33 @@
+# Copyright 2009-2011 Ram Rachum.
+# This program is distributed under the LGPL2.1 license.
+
+'''
+This module defines the `cute_select_more` script.
+
+See its documentation for more information.
+'''
+
+from __future__ import with_statement
+
+import os.path, sys; sys.path.append(os.path.dirname(__file__))
+
+import wingapi
+
+import shared
+
+
+SAFETY_LIMIT = 20
+
+
+def cute_select_more(editor=wingapi.kArgEditor):
+    
+    assert isinstance(editor, wingapi.CAPIEditor)
+    document = editor.GetDocument()
+    select_more = lambda: wingapi.gApplication.ExecuteCommand('select-more')
+
+    select_more()
+    for i in range(SAFETY_LIMIT):
+        selection_start, _ = editor.GetSelection()
+        if document.GetCharRange(selection_start-1, selection_start) == '.':
+            select_more()
+    
