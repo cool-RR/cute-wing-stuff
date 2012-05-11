41a57fc5b4f1f03ba6e585d824c8d2fdad4dadf1
Ram Rachum <ram@rachum.com>
2012-05-11 19:14:53 +0200

    -

--- scripts/frame_up_to_project.py
+++ scripts/frame_up_to_project.py
@@ -23,29 +23,21 @@ def frame_up_to_project():
     debugger = application.GetDebugger()
     all_project_files = project.GetAllFiles()
     _current_run_state = debugger.GetCurrentRunState()
-    _stack_frame = current_run_state.GetStackFrame()
-    _stack = current_run_state.GetStack()
+    _thread_id, _frame_index = _current_run_state.GetStackFrame()
+    _stack = _current_run_state.GetStack()
     
     file_paths = [file_path for file_path, _, _, _, _ in
                   stack[:_stack_frame+1]]
     
     file_paths_in_project = filter(
-        lambda file_path: file_path in all_project_files,
+        lambda _, file_path: file_path in all_project_files,
         enumerate(file_paths)
     )
     
     if not file_paths_in_project:
         return 
-    last_file_path_in_project = file_paths_in_project[-1]
+    index_of_last_file_path_in_project = file_paths_in_project[-1][0]
     
-        
-        
-    while application.CommandAvailable('frame-up') and \
-                                                not _are_we_in_project_frame():
-        
-    frame_up_to_project
-    ExecuteCommand('frame-show')
-    
-    ExecuteCommand('beginning-of-line-text')
-    
+    _current_run_state.SetStackFrame(_thread_id,
+                                     index_of_last_file_path_in_project)
     
