deddf68759b1a009ebd9597344daca400cfcd27a
Ram Rachum <ram@rachum.com>
2012-05-11 19:10:01 +0200

    -

--- scripts/frame_up_to_project.py
+++ scripts/frame_up_to_project.py
@@ -21,23 +21,31 @@ def frame_up_to_project():
     application = wingapi.gApplication
     project = application.GetProject()
     debugger = application.GetDebugger()
+    all_project_files = project.GetAllFiles()
+    _current_run_state = debugger.GetCurrentRunState()
+    _stack_frame = current_run_state.GetStackFrame()
+    _stack = current_run_state.GetStack()
     
-    print (vars(debugger.GetRunStates()[0]))
-    return 
-    #all_project_files = project.GetAllFiles()
+    file_paths = [file_path for file_path, _, _, _, _ in
+                  stack[:_stack_frame+1]]
     
+    file_paths_in_project = filter(
+        lambda file_path: file_path in all_project_files,
+        enumerate(file_paths)
+    )
     
+    if not file_paths_in_project:
+        return 
+    last_file_path_in_project = file_paths_in_project[-1]
     
-    #def _are_we_in_project_frame():
-        #''' '''
-        #wingapi.gApplication
         
-    #while application.CommandAvailable('frame-up') and \
-                                                #not _are_we_in_project_frame():
         
-    #frame_up_to_project
-    #ExecuteCommand('frame-show')
+    while application.CommandAvailable('frame-up') and \
+                                                not _are_we_in_project_frame():
+        
+    frame_up_to_project
+    ExecuteCommand('frame-show')
     
-    #ExecuteCommand('beginning-of-line-text')
+    ExecuteCommand('beginning-of-line-text')
     
     
