bc5c2b85fb8fde4db6287468495e06af8a0153f8
Ram Rachum <ram@rachum.com>
2012-05-29 14:50:33 +0300

    -

--- README.markdown
+++ README.markdown
@@ -232,18 +232,18 @@ of the text.
 Suggested key combination: `Shift-F11`
 
 
-## go-up-to-lowest-project-frame ##
+## go-up-to-project-frame ##
 
-Go to the lowest frame that's on project file rather than external module.
+Go up one frame in the debugger, skipping any non-project frames.
 
 Did you ever have Wing stop on an exception, and then drop you in code that
 belongs to an external module? This is often annoying, because you want to
 figure out what you did wrong on *your* code, and the external module is
 usually not to blame.
 
-`go-up-to-lowest-project-frame` to the rescue! Invoke this script while debugging
-in order to be taken to the lowest stack frame that's on a project file rather
-than an external module.
+`go-up-to-project-frame` to the rescue! Invoke this script while debugging
+in order to be taken to the closest higher stack frame that's on a project
+file rather than an external module.
     
 Suggested key combination: `Alt-F11`
     
--- scripts/go_up_to_project_frame.py
+++ scripts/go_up_to_project_frame.py
@@ -2,7 +2,7 @@
 # This program is distributed under the LGPL2.1 license.
 
 '''
-This module defines the `go_up_to_lowest_project_frame` script.
+This module defines the `go_up_to_project_frame` script.
 
 See its documentation for more information.
 '''
@@ -24,18 +24,18 @@ def _normalize_path(path):
         return os.path.realpath(path).lower()
 
 
-def go_up_to_lowest_project_frame(application=wingapi.gApplication):
+def go_up_to_project_frame(application=wingapi.gApplication):
     '''
-    Go to the lowest frame that's on project file rather than external module.
+    Go up one frame in the debugger, skipping any non-project frames.
 
     Did you ever have Wing stop on an exception, and then drop you in code that
     belongs to an external module? This is often annoying, because you want to
     figure out what you did wrong on *your* code, and the external module is
     usually not to blame.
     
-    `go-up-to-lowest-project-frame` to the rescue! Invoke this script while
-    debugging in order to be taken to the lowest stack frame that's on a
-    project file rather than an external module.
+    `go-up-to-project-frame` to the rescue! Invoke this script while debugging
+    in order to be taken to the closest higher stack frame that's on a project
+    file rather than an external module.
     '''
     project = application.GetProject()
     debugger = application.GetDebugger()
@@ -49,14 +49,17 @@ def go_up_to_lowest_project_frame(application=wingapi.gApplication):
     
     file_paths = [_normalize_path(file_path) for file_path, _, _, _, _ in
                   _stack]
-    
-    file_paths_in_project = filter(
-        lambda (_, file_path): file_path in all_project_files,
+    print(_frame_index)
+    file_paths_in_project_above_current_frame = filter(
+        lambda (i, file_path): file_path in all_project_files and
+                                                              i < _frame_index,
         enumerate(file_paths)
     )
-    if not file_paths_in_project:
+    print(len(file_paths_in_project_above_current_frame))
+    if not file_paths_in_project_above_current_frame:
         return 
-    index_of_last_file_path_in_project = file_paths_in_project[-1][0]
+    index_of_last_file_path_in_project = \
+                               file_paths_in_project_above_current_frame[-1][0]
     
     _current_run_state.SetStackFrame(_thread_id,
                                      index_of_last_file_path_in_project)
@@ -68,5 +71,5 @@ def _available(application=wingapi.gApplication):
         wingapi.gApplication.GetDebugger().GetCurrentRunState().GetStack()
     )
 
-go_up_to_lowest_project_frame.available = _available
+go_up_to_project_frame.available = _available
 
