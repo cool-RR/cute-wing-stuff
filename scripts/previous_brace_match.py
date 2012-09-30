21f2d20a177a8db106e928706206b724d5376670
Ram Rachum <ram@rachum.com>
2012-09-30 10:57:02 +0200

    -

--- scripts/previous_brace_match.py
+++ scripts/previous_brace_match.py
@@ -33,17 +33,16 @@ def previous_brace_match(editor=wingapi.kArgEditor):
     '''
     assert isinstance(editor, wingapi.CAPIEditor)
     document = editor.GetDocument()
-    with shared.ScrollRestorer(editor):
-        document_text = document.GetText()
-        _, caret_position = editor.GetSelection()
-        closing_brace_position = max((
-            document_text.rfind(')', 0, caret_position - 1), 
-            document_text.rfind(']', 0, caret_position - 1), 
-            document_text.rfind('}', 0, caret_position - 1)
-        ))
-        print(closing_brace_position)
-        if closing_brace_position == -1:
-            return
-        new_position = closing_brace_position
-        editor.SetSelection(new_position, new_position)
-        editor.ExecuteCommand('brace-match')
+    document_text = document.GetText()
+    _, caret_position = editor.GetSelection()
+    closing_brace_position = max((
+        document_text.rfind(')', 0, caret_position - 1), 
+        document_text.rfind(']', 0, caret_position - 1), 
+        document_text.rfind('}', 0, caret_position - 1)
+    ))
+    print(closing_brace_position)
+    if closing_brace_position == -1:
+        return
+    new_position = closing_brace_position
+    editor.SetSelection(new_position, new_position)
+    editor.ExecuteCommand('brace-match')
