f056c578cdc9d35ab4b8c05ebe861e92670dc150
Ram Rachum <ram@rachum.com>
2013-01-22 01:25:30 +0200

    -

--- scripts/deep_to_var.py
+++ scripts/deep_to_var.py
@@ -24,7 +24,7 @@ getitem_pattern = re.compile(r'''\[['"]([a-zA-Z_][0-9a-zA-Z_]*)['"]\]$''')
 ### Defining `getter_pattern`: ################################################
 #                                                                             #
 getter_verbs = ('get', 'calculate', 'identify', 'fetch', 'make', 'create',
-                'grant', 'open')
+                'grant', 'open', 'determine', 'download', 'obtain', 'fetch')
 
 getter_verb = '(?:%s)' % (
     '|'.join(
