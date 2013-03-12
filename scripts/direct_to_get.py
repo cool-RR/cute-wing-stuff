f32df9f154a0f7d36cbc8413e796a12e45818013
Ram Rachum <ram@rachum.com>
2013-03-12 19:52:55 +0200

    -

--- scripts/direct_to_get.py
+++ scripts/direct_to_get.py
@@ -20,16 +20,15 @@ import wingapi
 import shared
 
 
-caret_token = 'mUwMzJiNDg3MGE4NGE0ZmFjZGJkMGQ2NTNlNGQ4ZjQ5YTZjMGI1YzYwMmY0YjUw'
+#caret_token = 'mUwMzJiNDg3MGE4NGE0ZmFjZGJkMGQ2NTNlNGQ4ZjQ5YTZjMGI1YzYwMmY0YjUw'
 
 pattern = re.compile(
-    r'''.*?(?P<square_brackets>\[(?P<key_head>[^\]]*)'''
-    r'''%s(?P<key_tail>[^\]]*)\]).*''' % caret_token
+    r'''.*?(?P<square_brackets>\[(?P<key>.*?)\])$'''
 )
 
 def dict_direct_to_get(editor=wingapi.kArgEditor):
     '''
-    Suggested key combination: 
+    Suggested key combination: Insert Ctrl-G
     '''
     assert isinstance(editor, wingapi.CAPIEditor)
     document = editor.GetDocument()
@@ -39,24 +38,38 @@ def dict_direct_to_get(editor=wingapi.kArgEditor):
     current_line_number = document.GetLineNumberFromPosition(current_position)
     line_start = document.GetLineStart(current_line_number)
     line_end = document.GetLineEnd(current_line_number)
-    pimped_line_text = ''.join(line_start,
-                               caret_token,
-                               line_end)
+    line_head = document.GetCharRange(line_start, current_position)
+    line_tail = document.GetCharRange(current_position, line_end)
     
-    match = pattern.match(pimped_line_text)
+    if ']' not in line_tail:
+        print('''']' not in line_tail''')
+        return
+    
+    first_closing_bracket_position = line_tail.find(']') + len(line_head) + \
+                                                                     line_start
+    
+    text_until_closing_bracket = document.GetCharRange(
+        line_start,
+        first_closing_bracket_position + 1
+    )
+    
+    match = pattern.match(text_until_closing_bracket)
     if not match:
+        print('''no match''')
+        print('{{{%s}}}' % text_until_closing_bracket)
         return
     else: # we have a match
-        
+        print(match.group('key'))
+        print(match.group('square_brackets'))
     
-    with shared.UndoableAction(document):
-        start, end = shared.select_current_word(editor)    
-        variable_name = document.GetCharRange(start, end)
-        result_string = 'self.%s = %s' % (variable_name, variable_name)
-        document.DeleteChars(start, end - 1)
-        document.InsertChars(start, result_string)
-        editor.SetSelection(start + len(result_string),
-                            start + len(result_string))
-        editor.ExecuteCommand('new-line')
+    #with shared.UndoableAction(document):
+        #start, end = shared.select_current_word(editor)    
+        #variable_name = document.GetCharRange(start, end)
+        #result_string = 'self.%s = %s' % (variable_name, variable_name)
+        #document.DeleteChars(start, end - 1)
+        #document.InsertChars(start, result_string)
+        #editor.SetSelection(start + len(result_string),
+                            #start + len(result_string))
+        #editor.ExecuteCommand('new-line')
     
         
