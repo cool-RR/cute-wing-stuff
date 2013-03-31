af3ef568b13050c6a52d5e6d6a0df8d3dfaaa8c3
Ram Rachum <ram@rachum.com>
2013-03-31 03:30:53 +0300

    -

--- scripts/cute_start_select_line.py
+++ scripts/cute_start_select_line.py
@@ -33,3 +33,20 @@ def cute_start_select_line(editor=wingapi.kArgEditor):
     wingapi.gApplication.ExecuteCommand('start-select-line')
     editor.SetSelection(new_selection_start, new_selection_end)
     
+
+
+def _available(editor=wingapi.kArgEditor):
+    assert isinstance(editor, wingapi.CAPIEditor)
+    processed = editor.fEditor.GetCommandMap()._GetCommand('start_select_line')
+    x = editor.CommandAvailable('start-select-line')
+    with open(r'G:\Dropbox\Desktop\file.txt', mode='a') as log_file:
+        log_file.write(str((x, processed, editor)))
+    return True
+    return x
+
+#cute_start_select_line.available = (
+    #_a
+    ##lambda application:
+                     ##wingapi.gApplication.CommandAvailable('start-select-line')
+#)
+
