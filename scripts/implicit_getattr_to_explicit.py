93aea753fdaf7037795a760b360dff270502ceff
Ram Rachum <ram@rachum.com>
2013-06-02 01:05:52 +0300

    Merge branch 'master'

--- scripts/implicit_getattr_to_explicit.py
+++ scripts/implicit_getattr_to_explicit.py
@@ -12,6 +12,7 @@ from __future__ import with_statement
 
 import os.path, sys; sys.path.append(os.path.dirname(__file__))
 
+import bisect
 import re
 import inspect
 import logging
@@ -29,8 +30,11 @@ pattern = re.compile(
     )
 )
 
-def _get_matches():
-    
+def _get_matches(document):
+    assert isinstance(document, wingapi.CAPIDocument)
+    document_text = shared.get_text(document)
+    return tuple(pattern.finditer(document_text))
+
 
 def implicit_getattr_to_explicit(editor=wingapi.kArgEditor):
     '''
@@ -43,52 +47,28 @@ def implicit_getattr_to_explicit(editor=wingapi.kArgEditor):
     
     
     _, current_position = editor.GetSelection()
-    head = max(current_position - 100, 0)
-    tail = min(current_position + 100, document.GetLength())
+    #head = max(current_position - 100, 0)
+    #tail = min(current_position + 100, document.GetLength())
     
-    text = document.GetCharRange(head, tail)
+    #text = document.GetCharRange(head, tail)
     
-    matches = list(re.finditer(pattern, text))
-
-    logging.error('Printing matches:')
-    for match in matches:
-        logging.error(match.string[match.start():match.end()])
-        
-    return
+    matches = _get_matches(document)
+    match_spans = tuple(match.span(0) for match in matches)
+    candidate_index = max(
+        bisect.bisect_left(match_spans, (current_position, 0)) - 1,
+        0
+    )
+    candidate_span = match_spans[candidate_index]
+    candidate = matches[candidate_index]
+    #if candidate[0] <= current_position <= candidate[1]:
+        #editor.SetSelection(*candidate)
+    #else:
+        #print(candidate)
+        #try:
+            #print(shared.get_text(document)[candidate[0]:candidate[1]])
+        #except:
+            #pass
     
-    #if ']' not in line_tail:
-        ##print('''']' not in line_tail''')
-        #return
-    
-    #first_closing_bracket_position = line_tail.find(']') + len(line_head) + \
-                                                                     #line_start
-    
-    #text_until_closing_bracket = document.GetCharRange(
-        #line_start,
-        #first_closing_bracket_position + 1
-    #)
-    
-    #match = pattern.match(text_until_closing_bracket)
-    #if not match:
-        ##print('''no match''')
-        ##print('{{{%s}}}' % text_until_closing_bracket)
-        #return
-    #else: # we have a match
-        #square_brackets_position = \
-            #line_text.find(match.group('square_brackets'))
-        #text_to_insert = '.get(%s, None)' % match.group('key')
-        #new_line_text = line_text.replace(
-            #match.group('square_brackets'), text_to_insert
-        #)
-        #none_start = line_start + square_brackets_position + \
-                                                        #len(text_to_insert) - 5
-        #none_end = line_start + square_brackets_position + \
-                                                        #len(text_to_insert) - 1
-
-        #with shared.UndoableAction(document):
-            #document.DeleteChars(line_start, line_end - 1)
-            ## The `- 1` above is necessary for \n-documents.
-            #document.InsertChars(line_start, new_line_text)
-            #editor.SetSelection(none_start, none_end)
-    
-        
+    new_text = 'getattr(%s, %s, None)' % (candidate.group(0),
+                                          candidate.group(1))
+    print(new_text)
