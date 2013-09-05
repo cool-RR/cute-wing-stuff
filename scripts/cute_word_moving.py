00f1b4ed4c9622d76e4058d6378e22316c00cc41
Ram Rachum <ram@rachum.com>
2013-09-05 22:45:12 +0300

    -

---
+++ scripts/cute_word_moving.py
@@ -0,0 +1,109 @@
+# Copyright 2009-2013 Ram Rachum.
+# This program is distributed under the MIT license.
+
+
+from __future__ import with_statement
+
+import os.path, sys; sys.path.append(os.path.dirname(__file__))
+
+import sys
+import inspect
+import re
+import string
+
+import wingapi
+
+import shared
+
+most_punctuation = r'''!"#$%&'()*+,-./:;<=>?@[\]^`{|}~'''
+# (This is `string.punctuation` without `_`.)
+
+most_punctuation_re_set = r'''[!"#$%&'()*+,\-./:;<=>?@[\\\]^`{|}~]'''
+
+word_pattern = re.compile(
+    r'''^(?:'''
+        r'''[ \t\n\r]*''' # Whitespace between any two words
+        r'''[!"#$%&'()*+,\-./:;<=>?@[\\\]^`{|}~]*''' # Punctuation except `_`
+        r'''(?:''' # Variable name
+            r'''('''
+                r'''(?:_+[A-Za-z0-9]*(?:_+(?![A-Za-z0-9_]))?)'''
+                r'''|'''
+                r'''(?:[A-Za-z]+(?:_+(?![A-Za-z0-9_]))?)'''
+            r''')'''
+        r''')'''
+    r''')*'''
+    r'''[ \t\n\r]*$'''
+)
+
+def _type_string(string):
+    assert shared.autopy_available
+    import autopy.key
+    for character in string:
+        if character in _characters_that_need_shift:
+            autopy.key.tap(character, autopy.key.MOD_SHIFT)
+        else:
+            autopy.key.tap(character)
+        autopy.key.tap(135) # F24 for making AHK not interfere
+        autopy.key.tap(35) # `End` for making AHK not interfere
+
+
+def _cute_general_replace(command_name,
+                          editor=wingapi.kArgEditor,
+                          app=wingapi.kArgApplication):
+    assert isinstance(editor, wingapi.CAPIEditor)
+    assert isinstance(app, wingapi.CAPIApplication)
+    selection_start, selection_end = editor.GetSelection()
+    selection = editor.GetDocument().GetCharRange(selection_start,
+                                                  selection_end)
+    
+    if selection:
+        wingapi.gApplication.SetClipboard(selection)
+        editor.SetSelection(selection_start, selection_start)
+        app.ExecuteCommand(command_name)
+        if shared.autopy_available:
+            import autopy.key
+            autopy.key.toggle(autopy.key.K_ALT, False)
+            autopy.key.toggle(autopy.key.K_SHIFT, False)
+            autopy.key.toggle(autopy.key.K_CONTROL, False)
+            autopy.key.toggle(autopy.key.K_META, False)
+            #_type_string(selection)
+            #autopy.key.tap(autopy.key.K_ESCAPE)
+            #autopy.key.toggle(autopy.key.K_ALT, False)
+            #autopy.key.tap(TAB_KEY)
+            #autopy.key.tap('l', autopy.key.MOD_ALT)
+            #autopy.key.tap('v', autopy.key.MOD_CONTROL)
+            #autopy.key.tap('a', autopy.key.MOD_CONTROL)
+            
+        
+    else: # not selection
+        app.ExecuteCommand(command_name)
+        
+        
+def cute_query_replace(editor=wingapi.kArgEditor,
+                       app=wingapi.kArgApplication):
+    '''
+    Improved version of `query-replace` for finding and replacing in document.
+    
+    BUGGY: If text is selected, it will be used as the text to search for, and
+    the contents of the clipboard will be offered as the replace value.
+    
+    Implemented on Windows only.
+    
+    Suggested key combination: `Alt-Comma`
+    '''
+    return _cute_general_replace('query-replace', editor=editor, app=app)
+
+
+def cute_replace_string(editor=wingapi.kArgEditor,
+                       app=wingapi.kArgApplication):
+    '''
+    Improved version of `replace-string` for finding and replacing in document.
+    
+    BUGGY: If text is selected, it will be used as the text to search for, and
+    the contents of the clipboard will be offered as the replace value.
+    
+    Implemented on Windows only.
+    
+    Suggested key combination: `Alt-Period`
+    '''    
+    return _cute_general_replace('replace-string', editor=editor, app=app)
