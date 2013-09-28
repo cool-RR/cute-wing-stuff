162973280fe174e6d97821c6bfbf5a577c2c86ca
Ram Rachum <ram@rachum.com>
2013-09-28 18:38:13 +0300

    -

--- scripts/django_toggle_between_view_and_template.py
+++ scripts/django_toggle_between_view_and_template.py
@@ -24,7 +24,7 @@ shorten_template_file_path_pattern = re.compile(
 )
 
 
-def django_switch_between_view_and_template():
+def django_toggle_between_view_and_template():
     
     app = wingapi.gApplication
     editor = app.GetActiveEditor()
@@ -37,21 +37,17 @@ def django_switch_between_view_and_template():
     
     
     if file_name.endswith('.py'):
-        print('Is Python file')
         match = template_name_pattern.search(document_text)
         if match:
-            print('Got match')            
             template_partial_file_path = match.group(1)
             matching_file_paths = [
                 file_path for file_path in all_file_paths
                 if template_partial_file_path in file_path.replace('\\', '/')
             ]
             if matching_file_paths:
-                print('Got matching file path')            
                 matching_file_path = matching_file_paths[0]
                 app.OpenEditor(matching_file_path, raise_window=True)
     elif file_name.endswith('.html'):
-        print('Is HTML file')
         short_file_path = shorten_template_file_path_pattern.match(
                                          file_path.replace('\\', '/')).group(1)
         specifies_our_template_pattern = re.compile(
@@ -61,7 +57,6 @@ def django_switch_between_view_and_template():
             file_path for file_path in all_file_paths
             if view_file_path_pattern.search(file_path)
         ]
-        print(all_view_file_paths)
             
         matching_file_paths_iterator = (
             file_path for file_path in all_view_file_paths if
@@ -70,7 +65,6 @@ def django_switch_between_view_and_template():
         )
         try:
             matching_file_path = next(matching_file_paths_iterator)
-            print('Got match: %s' % matching_file_path)
         except StopIteration:
             return
         app.OpenEditor(matching_file_path, raise_window=True)
