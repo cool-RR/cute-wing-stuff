ab6e77534f8dc68cad3327715521f62f1eaa639e
Ram Rachum <ram@rachum.com>
2017-04-11 20:11:11 +0300

    WIP f-strings

--- scripts/edit_string.py
+++ scripts/edit_string.py
@@ -226,11 +226,11 @@ def edit_string():
     layout.addLayout(sub_layout)
     
     replacing_old_string = \
-        string_selecting._is_position_on_string(editor, selection_start)
+               string_selecting._is_position_on_string(editor, selection_start)
     if replacing_old_string:
-        old_string_ranges = \
-           string_selecting._find_string_from_position(editor, selection_start,
-                                                       multiline=True)
+        old_string_token, old_string_range = string_selecting. \
+                            _find_string_from_position(editor, selection_start)
+        gotta do the multiline
         old_string_raw = document.GetCharRange(old_string_ranges[0][0],
                                                old_string_ranges[-1][1])
         old_string = ast.literal_eval('(%s)' % old_string_raw)
