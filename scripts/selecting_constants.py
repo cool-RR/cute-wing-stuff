bfa54e666d1eaa92cc323bb3793f0b07089535bc
Ram Rachum <ram@rachum.com>
2020-03-08 18:45:44 +0200

    Fix regex bug

--- scripts/selecting_constants.py
+++ scripts/selecting_constants.py
@@ -14,8 +14,8 @@ import _ast
 
 import os.path, sys
 sys.path += [
-    os.path.dirname(__file__), 
-    os.path.join(os.path.dirname(__file__), 'third_party.zip'), 
+    os.path.dirname(__file__),
+    os.path.join(os.path.dirname(__file__), 'third_party.zip'),
 ]
 
 
@@ -26,7 +26,7 @@ import shared
 
 
 constant_pattern = re.compile(
-    r'''(?<![A-za-z0-9_])(?:_[0-9_]*)?(?:[A-Z][0-9_]*){2,}(?![A-za-z0-9_])'''
+    r'''(?<![A-Za-z0-9_])(?:_[0-9_]*)?(?:[A-Z][0-9_]*){2,}(?![A-Za-z0-9_])'''
 )
 
 
@@ -49,7 +49,7 @@ def _get_relevant_constant_positions(editor, caret_position):
             last_start_position, last_end_position = \
                                                    start_position, end_position
     else:
-        next_start_position = next_end_position = None        
+        next_start_position = next_end_position = None
     return ((last_start_position, last_end_position),
             (next_start_position, next_end_position))
 
@@ -58,40 +58,39 @@ def select_next_constant(editor=wingapi.kArgEditor,
                        app=wingapi.kArgApplication):
     '''
     Select the next (or current) constant in the document.
-    
+
     Constant means a name in all caps, like DEBUG or LOGIN_REDIRECT_URL.
-    
+
     Suggested key combination: `Ctrl-Alt-O`
     '''
     assert isinstance(editor, wingapi.CAPIEditor)
     document = editor.GetDocument()
 
     caret_position = editor.GetSelection()[1] + 1
-    
+
     _, next_constant_position = _get_relevant_constant_positions(editor,
                                                             caret_position)
     if next_constant_position != (None, None):
         app.ExecuteCommand('set-visit-history-anchor')
         editor.SetSelection(*next_constant_position)
 
-    
+
 def select_prev_constant(editor=wingapi.kArgEditor,
                        app=wingapi.kArgApplication):
     '''
     Select the previous constant in the document.
-    
+
     Constant means a name in all caps, like DEBUG or LOGIN_REDIRECT_URL.
-    
+
     Suggested key combination: `Ctrl-Alt-Shift-O`
     '''
     assert isinstance(editor, wingapi.CAPIEditor)
     document = editor.GetDocument()
-    
+
     caret_position = editor.GetSelection()[0]
-    
+
     prev_constant_position, _  = _get_relevant_constant_positions(editor,
                                                              caret_position)
     if prev_constant_position != (None, None):
         app.ExecuteCommand('set-visit-history-anchor')
         editor.SetSelection(*prev_constant_position)
-    
