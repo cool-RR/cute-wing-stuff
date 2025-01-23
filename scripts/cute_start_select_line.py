f943d2c3c024bfbfda36ca3e44e86a59e53dd3dd
Ram Rachum <ram@rachum.com>
2025-01-23 12:42:24 +0200

    fix cute-start-select-line for unicode

--- scripts/cute_start_select_line.py
+++ scripts/cute_start_select_line.py
@@ -30,47 +30,46 @@ def cute_start_select_line():
     editor = wingapi.gApplication.GetActiveEditor()
     assert isinstance(editor, wingapi.CAPIEditor)
     document = editor.GetDocument()
-    selection_start, selection_end = shared.get_selection_unicode(editor)
-    selection_start_line = document.GetLineNumberFromPosition(selection_start)
-    selection_end_line = document.GetLineNumberFromPosition(selection_end)
+    document_text = document.GetText()
+    utf8_selection_start, utf8_selection_end = editor.GetSelection()
+    selection_start_line = document.GetLineNumberFromPosition(utf8_selection_start)
+    selection_end_line = document.GetLineNumberFromPosition(utf8_selection_end)
 
-    new_selection_start = document.GetLineStart(selection_start_line)
+    utf8_new_selection_start = document.GetLineStart(selection_start_line)
 
     ###########################################################################
     #                                                                         #
-    raw_new_selection_end = document.GetLineEnd(selection_end_line)
-    two_characters_after = document.GetText()[raw_new_selection_end :
-                                              min(raw_new_selection_end + 2, document.GetLength())]
-    if two_characters_after:
-        if two_characters_after[0] == '\n':
-            new_selection_end = raw_new_selection_end + 1
-        elif two_characters_after[0] == '\r':
-            if len(two_characters_after) == 2 and \
-                                               two_characters_after[1] == '\n':
-                new_selection_end = raw_new_selection_end + 2
+    raw_utf8_new_selection_end = document.GetLineEnd(selection_end_line)
+    utf8_two_characters_after = document_text[raw_utf8_new_selection_end :
+                                         min(raw_utf8_new_selection_end + 2, document.GetLength())]
+    if utf8_two_characters_after:
+        if utf8_two_characters_after[0] == '\n':
+            utf8_new_selection_end = raw_utf8_new_selection_end + 1
+        elif utf8_two_characters_after[0] == '\r':
+            if len(utf8_two_characters_after) == 2 and \
+                                               utf8_two_characters_after[1] == '\n':
+                utf8_new_selection_end = raw_utf8_new_selection_end + 2
             else:
-                new_selection_end = raw_new_selection_end + 1
+                utf8_new_selection_end = raw_utf8_new_selection_end + 1
         else:
-            new_selection_end = raw_new_selection_end
+            utf8_new_selection_end = raw_utf8_new_selection_end
     else:
-        new_selection_end = raw_new_selection_end
+        utf8_new_selection_end = raw_utf8_new_selection_end
     #                                                                         #
     ###########################################################################
 
-    _, caret_position = editor.GetAnchorAndCaret()
-    if caret_position == selection_end:
-        arguments = (new_selection_start, new_selection_end)
+    _, utf8_caret_position = editor.GetAnchorAndCaret()
+    if utf8_caret_position == utf8_selection_end:
+        arguments = (utf8_new_selection_start, utf8_new_selection_end)
     else:
-        assert caret_position == selection_start
-        arguments = (new_selection_end, new_selection_start)
+        assert utf8_caret_position == utf8_selection_start
+        arguments = (utf8_new_selection_end, utf8_new_selection_start)
 
     wingapi.gApplication.ExecuteCommand('start-select-line')
-    shared.set_selection_unicode(editor, *arguments)
+    editor.SetSelection(*arguments)
 
 
 
-cute_start_select_line.available = (
-    lambda :
-                     wingapi.gApplication.CommandAvailable('start_select_line')
-)
+cute_start_select_line.available = \
+                                  lambda: wingapi.gApplication.CommandAvailable('start_select_line')
 
